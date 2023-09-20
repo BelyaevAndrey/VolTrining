9c41e4e9262c5803350c86451bb7edfa6cb13fdb
BelyaevAndrey <andreybelyaevalexandrovich@gmail.com>
2023-09-20 09:51:27 +0200

    Добавьте файлы проекта.

---
+++ VolClient/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VolClient.Models;
+
+namespace VolClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<VolClientModel> _volClientList;
+            
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _volClientList = new BindingList<VolClientModel>();
+            {
+                new VolClientModel() {Text = "test"};
+                new VolClientModel() {Text = "gfgfg"};
+                new VolClientModel() {Text= "test 2", IsDone = true};
+            };
+
+            dgVolClientList.ItemsSource = _volClientList;
+            _volClientList.ListChanged += _volClientList_ListChanged;
+        }
+
+        private void _volClientList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch (e.ListChangedType)
+            {
+                case ListChangedType.Reset:
+                    break;
+                case ListChangedType.ItemAdded:
+                    break;
+                case ListChangedType.ItemDeleted:
+                    break;
+                case ListChangedType.ItemMoved:
+                    break;
+                case ListChangedType.ItemChanged:
+                    break;
+                case ListChangedType.PropertyDescriptorAdded:
+                    break;
+                case ListChangedType.PropertyDescriptorDeleted:
+                    break;
+                case ListChangedType.PropertyDescriptorChanged:
+                    break;
+            }
+        }
+    }
+}
+
---
+++ VolClient/Models/VolClientModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace VolClient.Models
+{
+    class VolClientModel: INotifyPropertyChanged
+    {
+        private bool _isDone;
+        private string _text;
+        public DateTime TimeTable { get; set; } = DateTime.Now;
+        public bool IsDone
+        {
+            get { return _isDone; }
+            set { _isDone = value; }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
